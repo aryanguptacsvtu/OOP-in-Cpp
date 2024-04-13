b96033ae93ec8bf7995a82f80331ad445bb8326a
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2024-04-13 10:54:23 +0530

    Program2.cpp

---
+++ EXPERIMENTS.cpp/Prog2.cpp
@@ -0,0 +1,16 @@
+// Program to add two numbers.
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+
+    cout << "Enter value of a:";
+    cin >> a;
+    cout << "Enter value of b:";
+    cin >> b;
+
+    cout << "The sum of a & b is:" << a + b;
+    return 0;
+}
