ac16eb09f13c6051568942366f98ad7e12a40271
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2024-08-20 18:48:08 +0530

    Code4

---
+++ UNIT 3/Code4.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class Time
+{
+    int hour, mins;
+
+public:
+    Time()
+    {
+        hour = 0;
+        mins = 0;
+    }
+    Time(int t)
+    {
+        hour = t / 60;
+        mins = t % 60;
+    }
+
+    operator int() const      // Conversion Function
+    {
+        return hour * 60 + mins;
+    }
+
+    void Display()
+    {
+        cout << "Time:" << hour << "hrs and " << mins << "mins" << endl;
+    }
+};
+
+int main()
+{
+    Time T1;
+    int dur = 95;
+
+    T1 = dur;
+    T1.Display();
+
+    int totalMinutes = T1;
+    cout << "Total minutes:" << totalMinutes << "mins\n";
+    return 0;
+}
