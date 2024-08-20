98bda09bad052f413d9b5fcc447128765fecb43c
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2024-08-20 19:22:05 +0530

    Code7

---
+++ UNIT 3/Code7.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+class Parent
+{
+public:
+    int id_p;
+};
+
+class Child : public Parent
+{
+public:
+    int id_c;
+};
+
+int main()
+{
+    Child obj1;
+    obj1.id_c = 7;
+    obj1.id_p = 91;
+
+    cout << "Child id is:" << obj1.id_c << endl;
+    cout << "Parent is is:" << obj1.id_p;
+
+    return 0;
+}
