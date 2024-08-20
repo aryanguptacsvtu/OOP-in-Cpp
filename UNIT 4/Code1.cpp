85aa24375fef325ecf727d2354451fa74740faed
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2024-08-20 20:25:54 +0530

    Code1

---
+++ UNIT 4/Code1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int var1 = 3;
+    int var2 = 24;
+    int var3 = 17;
+
+    cout<<"Address of var1:"<<&var1<<endl;
+    cout<<"Address of var2:"<<&var2<<endl;
+    cout<<"Address of var3:"<<&var3<<endl;
+    return 0;
+}
