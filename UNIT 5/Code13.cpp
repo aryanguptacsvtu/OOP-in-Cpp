88dda726e591b54168a1594546e5651dcadd6d4c
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2024-08-21 15:08:25 +0530

    Code13

---
+++ UNIT 5/Code13.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stack>
+#include <queue>
+using namespace std;
+int main()
+{
+    stack<int> stk;
+    stk.push(1);
+
+    queue<int> q;
+    q.push(7);
+
+    priority_queue<int> pq;
+    pq.push(10);
+
+    return 0;
+}
