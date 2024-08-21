de8c761a2301defaff207a9c013a55fbcd77987d
aryanguptacsvtu <148983503+aryanguptacsvtu@users.noreply.github.com>
2024-08-21 14:24:58 +0530

    Code1

---
+++ UNIT 5/Code1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char x;
+
+    cin.get(x);
+
+    cout << "Output:" << endl;
+    cout << x << endl;
+    cout.put(x);
+
+    return 0;
+}
